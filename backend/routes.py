ea23a4c80539f3474723d4276859d1487cbd3389
linustws <linustws00@gmail.com>
2023-06-19 23:26:36 +0800

    start frontend

--- backend/routes.py
+++ backend/routes.py
@@ -1,29 +1,20 @@
-from flask import Flask, request, jsonify
-from models import LSTMModel
-
-app = Flask(__name__)
-
-# Instantiate the model
-model = LSTMModel()
+from flask import request, jsonify
+from models import model
+from app import app
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the input data from the request
-    data = request.json
+    input_data = request.form['input_field']
+    selected_option = request.form['dropdown']
+
+    if selected_option == 'Years':
+        num_months = int(input_data) * 12
+    else:
+        num_months = int(input_data)
 
     # Perform prediction using the model
-    prediction = model.predict(data)
+    prediction = model.predict(num_months)
 
     # Return the prediction as a JSON response
     return jsonify({'prediction': prediction})
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    # Perform health check
-    status = model.check_health()
-
-    # Return the health status as a JSON response
-    return jsonify({'status': status})
-
-if __name__ == '__main__':
-    app.run(debug=True)
