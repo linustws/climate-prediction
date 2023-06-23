ec67be3734b99672bcb0c7aba176b3ae7fab8efd
linustws <linustws00@gmail.com>
2023-06-23 10:22:15 +0800

    set up tailwind for svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: #F3F4F6; /* Use the hex code for gray-100 */
+    }
+</style>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
