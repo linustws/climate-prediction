395b10b10e6220fae5d5d24cb60d3a8bfe714d4b
linustws <linustws00@gmail.com>
2023-07-06 22:12:51 +0800

    clean up

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -16,9 +16,6 @@
         background-image: url("$lib/images/background.png");
         background-size: cover;
         background-position: center;
-        //display: flex;
-        //flex-direction: column;
-        //align-items: center;
         font-size: 1.5rem;
         font-weight: 300;
         position: fixed;
