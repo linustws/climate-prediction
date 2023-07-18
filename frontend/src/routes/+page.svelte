a4c7a7fb9a40a7d90f3014b91bfbd3ea5e6f1e3b
linustws <linustws00@gmail.com>
2023-07-18 21:24:23 +0800

    add predict button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,27 +1,16 @@
-<!--<html lang="en">-->
-<!--<body>-->
-<!--<div class="body">-->
-<!--    &lt;!&ndash; Content &ndash;&gt;-->
-<!--</div>-->
-<!--</body>-->
+<script>
+    import SparkleButton from '$lib/components/SparkleButton.svelte';
+</script>
 
-<!--<style lang="postcss">-->
-<!--    body {-->
-<!--        margin: 0;-->
-<!--        height: 100%;-->
-<!--        overflow: hidden-->
-<!--    }-->
+<div class="sparkle-button">
+<SparkleButton buttonText="Predict" />
+</div>
 
-<!--    .body {-->
-<!--        background-image: url("/src/lib/images/background.png");-->
-<!--        background-size: cover;-->
-<!--        background-position: center;-->
-<!--        min-height: 100vh;-->
-<!--        display: flex;-->
-<!--        flex-direction: column;-->
-<!--        align-items: center;-->
-<!--        font-size: 1.5rem;-->
-<!--        font-weight: 300;-->
-<!--    }-->
-<!--</style>-->
-<!--</html>-->
+<style lang="postcss">
+    .sparkle-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+</style>
