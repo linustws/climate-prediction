fc050e7ead8b0c8a515d7f998dea3df3d6aa19a8
linustws <linustws00@gmail.com>
2023-07-20 21:44:05 +0800

    make button bodydrop transparent and scroll to smooth

--- frontend/src/lib/components/SparkleButton.svelte
+++ frontend/src/lib/components/SparkleButton.svelte
@@ -5,21 +5,21 @@
 
 <div class="sparkle-button">
     <button>
-        <span class="spark" />
-        <span class="backdrop" />
+        <span class="spark"/>
+        <span class="backdrop"/>
         <svg class="sparkle" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"
-                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M6 14.25L5.741 15.285C5.59267 15.8785 5.28579 16.4206 4.85319 16.8532C4.42059 17.2858 3.87853 17.5927 3.285 17.741L2.25 18L3.285 18.259C3.87853 18.4073 4.42059 18.7142 4.85319 19.1468C5.28579 19.5794 5.59267 20.1215 5.741 20.715L6 21.75L6.259 20.715C6.40725 20.1216 6.71398 19.5796 7.14639 19.147C7.5788 18.7144 8.12065 18.4075 8.714 18.259L9.75 18L8.714 17.741C8.12065 17.5925 7.5788 17.2856 7.14639 16.853C6.71398 16.4204 6.40725 15.8784 6.259 15.285L6 14.25Z"
-                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M6.5 4L6.303 4.5915C6.24777 4.75718 6.15472 4.90774 6.03123 5.03123C5.90774 5.15472 5.75718 5.24777 5.5915 5.303L5 5.5L5.5915 5.697C5.75718 5.75223 5.90774 5.84528 6.03123 5.96877C6.15472 6.09226 6.24777 6.24282 6.303 6.4085L6.5 7L6.697 6.4085C6.75223 6.24282 6.84528 6.09226 6.96877 5.96877C7.09226 5.84528 7.24282 5.75223 7.4085 5.697L8 5.5L7.4085 5.303C7.24282 5.24777 7.09226 5.15472 6.96877 5.03123C6.84528 4.90774 6.75223 4.75718 6.697 4.5915L6.5 4Z"
-                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                  fill="black" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
         <span class="text">{buttonText}</span>
     </button>
-    <div class="bodydrop" />
+    <div class="bodydrop"/>
     <span aria-hidden="true" class="particle-pen">
-    {#each { length: 20 } as _}
+    {#each {length: 20} as _}
       <svg
               class="particle"
               viewBox="0 0 15 15"
@@ -54,6 +54,7 @@
     *:before {
         box-sizing: border-box;
     }
+
     :root {
         --transition: 0.25s;
         --spark: 1.8s;
@@ -62,9 +63,7 @@
     body {
         --active: 0;
         background: hsl(
-                260
-                calc(var(--active) * 97%)
-                6%
+                260 calc(var(--active) * 97%) 6%
         );
         display: grid;
         place-items: center;
@@ -77,18 +76,17 @@
     button {
         --cut: 0.1em;
         --active: 0;
-        --bg:
-                radial-gradient(
-                        40% 50% at center 100%,
-                        hsl(270 calc(var(--active) * 97%) 72% / var(--active)),
-                        transparent
-                ),
-                radial-gradient(
-                        80% 100% at center 120%,
-                        hsl(260 calc(var(--active) * 97%) 70% / var(--active)),
-                        transparent
-                ),
-                hsl(260 calc(var(--active) * 97%) calc((var(--active) * 44%) + 12%));
+        --bg: radial-gradient(
+                40% 50% at center 100%,
+                hsl(270 calc(var(--active) * 97%) 72% / var(--active)),
+                transparent
+        ),
+        radial-gradient(
+                80% 100% at center 120%,
+                hsl(260 calc(var(--active) * 97%) 70% / var(--active)),
+                transparent
+        ),
+        hsl(260 calc(var(--active) * 97%) calc((var(--active) * 44%) + 12%));
         background: var(--bg);
         font-size: 1.5rem;
         font-weight: 500;
@@ -101,10 +99,9 @@
         white-space: nowrap;
         border-radius: 100px;
         position: relative;
-        box-shadow:
-                0 0 calc(var(--active) * 6em) calc(var(--active) * 3em) hsl(260 97% 61% / 0.75),
-                0 0.05em 0 0 hsl(260 calc(var(--active) * 97%) calc((var(--active) * 50%) + 30%)) inset,
-                0 -0.05em 0 0 hsl(260 calc(var(--active) * 97%) calc(var(--active) * 60%)) inset;
+        box-shadow: 0 0 calc(var(--active) * 6em) calc(var(--active) * 3em) hsl(260 97% 61% / 0.75),
+        0 0.05em 0 0 hsl(260 calc(var(--active) * 97%) calc((var(--active) * 50%) + 30%)) inset,
+        0 -0.05em 0 0 hsl(260 calc(var(--active) * 97%) calc(var(--active) * 60%)) inset;
         transition: box-shadow var(--transition), scale var(--transition), background var(--transition);
         scale: calc(1 + (var(--active) * 0.1));
     }
@@ -137,6 +134,7 @@
             scale: var(--scale);
         }
     }
+
     .sparkle path:nth-of-type(1) {
         --scale: 0.5;
         --delay: 0.1;
@@ -225,12 +223,12 @@
     }
 
 
-
-    @supports(selector(:has(:is(+ *)))) {
+    @supports (selector(:has(:is(+ *)))) {
         body:has(button:is(:hover, :focus-visible)) {
             --active: 1;
             --play-state: running;
         }
+
         .bodydrop {
             display: none;
         }
@@ -242,16 +240,13 @@
     }
 
     .bodydrop {
-        background: hsl(
-                260
-                calc(var(--active) * 97%)
-                6%
+        background: hsla(
+                260 calc(var(--active) * 97%) 6% 0
         );
         position: fixed;
         inset: 0;
         z-index: -1
     }
-
 
 
     button:is(:hover, :focus-visible) {
